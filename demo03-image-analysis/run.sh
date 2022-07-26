1a003d689e0e3709e4eb4b9e2daf24d786f01361
Maxwell Duarte <maxmelodia@gmail.com>
2022-07-26 00:10:22 -0300

    feat: cria api para analisar imagens com AWS Rekognition
    Utiliza também o serviço AWS translate

---
+++ demo03-image-analysis/run.sh
@@ -0,0 +1,19 @@
+# instalar aws-sdk
+npm i aws-sdk
+
+# invocar na AWS
+sls invoke -f hello
+
+# invocar local (--l log)
+sls invoke local -f hello --l
+
+# logs
+sls logs -f hello --tail
+
+# remover
+sls remove
+
+# exemplo
+https://66zpyvom3g.execute-api.us-east-1.amazonaws.com/dev/analyse
+https://66zpyvom3g.execute-api.us-east-1.amazonaws.com/dev/analyse?imageUrl=
+https://66zpyvom3g.execute-api.us-east-1.amazonaws.com/dev/analyse?imageUrl=https://www.quecomem.com/wp-content/uploads/2022/01/O-que-o-ornitorrinco-come.jpg
